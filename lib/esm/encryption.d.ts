8572d1fcb104bd321c153c647411e05a4a3153a5
Shivam <singhshivamkr@gmail.com>
2022-02-06 12:37:37 +0530

    1.0.3

---
+++ lib/esm/encryption.d.ts
@@ -0,0 +1,29 @@
+/**
+ * Encrypts plaintext using AES-GCM with supplied password, for decryption with aesGcmDecrypt().
+ *                                                                      (c) Chris Veness MIT Licence
+ *
+ * @param   {String} plaintext - Plaintext to be encrypted.
+ * @param   {String} password - Password to use to encrypt plaintext.
+ * @returns {String} Encrypted ciphertext.
+ *
+ * @example
+ *   const ciphertext = await aesGcmEncrypt('my secret text', 'pw');
+ *   aesGcmEncrypt('my secret text', 'pw').then(function(ciphertext) { console.log(ciphertext); });
+ */
+export declare function aesGcmEncrypt(plaintext: string, password: string): Promise<string>;
+/**
+ * Decrypts ciphertext encrypted with aesGcmEncrypt() using supplied password.
+ *                                                                      (c) Chris Veness MIT Licence
+ *
+ * @param   {String} ciphertext - Ciphertext to be decrypted.
+ * @param   {String} password - Password to use to decrypt ciphertext.
+ * @returns {String} Decrypted plaintext.
+ *
+ * @example
+ *   const plaintext = await aesGcmDecrypt(ciphertext, 'pw');
+ *   aesGcmDecrypt(ciphertext, 'pw').then(function(plaintext) { console.log(plaintext); });
+ */
+export declare function aesGcmDecrypt({ ciphertext, password, }: {
+    ciphertext: string;
+    password: string;
+}): Promise<string>;
